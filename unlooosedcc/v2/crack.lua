c78617162f06d7411e5232ebc062f62cfc6cac3c
some11 <navitpeher@gmail.com>
2025-08-07 19:59:51 +0300

    Update crack.lua

--- unlooosedcc/v2/crack.lua
+++ unlooosedcc/v2/crack.lua
@@ -1,3 +1,3 @@
 
-loadstring(game:HttpGet("https://pastefy.app/ZVUTeVZw/raw",true))()
-loadstring(game:HttpGet("https://pastefy.app/MuJKIvU0/raw",true))()
+loadstring(game:HttpGet("https://pastefy.app/cxZnGDDH/raw",true))()
+loadstring(game:HttpGet("https://pastefy.app/p1WPE8X6/raw",true))()
